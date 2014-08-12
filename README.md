81424afcb8ab0b4e00d1885b7edc96a786721d47
Ethan Jennings <ethanhjennings@gmail.com>
2014-08-12 04:25:13 -0700

    Changed to use pty.js

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 #Terminal Portal
 
-This is a command line tool built using node.js which displays terminal output piped in from stdin on a read-only webpage that's also hosted by the same command. It's like the 'tee' command except it outputs to the web instead of to a file.
-
-Terminal colors are supported!
+This is a command line tool built using node.js which runs whatever it is given and hosts a webpage displaying the output.
 
 ## Installation
 
@@ -14,14 +12,10 @@ This will install it globally. Leave off the `-g` if you only want to install lo
 
 ## Usage
 
-If you just want to display a command that runs for a short amount of time you can pipe it directly, for example:
-```bash
-$ echo "Hello world!" | termportal
-```
+To display the command `top`:
 
-If the command is longer running and you want to see output in realtime you need to un-buffer the pipe which can be done with stdbuf, for example:
 ```bash
-$ stdbuf -oL -eL <your command> | termportal
+$ termportal top
 ```
 
 To view the web output go to:
@@ -29,13 +23,22 @@ To view the web output go to:
 http://localhost:4000
 ```
 
-Use the `-p` option if you want to use a different port.
+You can specify options before the command like this:
+
+```bash
+$ termportal --port 8000 top
+```
+
+## Options
+
+
+## Features
+-- terminal colors and cursor movement (like top uses) are supported.
 
 ## Issues
 - No security!
-- Commands like "top" that move the cursor stop displaying correctly after a while.
 
 ## Kudos
 
-- clientside terminal display uses [term.js](https://github.com/chjj/term.js)
+- this project relies on [pty.js](https://github.com/chjj/pty.js) to run the command in its own shell and [term.js](https://github.com/chjj/pty.js) to display the terminal output clientside
 - loading animation is a modified version taken from [SpinKit](https://github.com/tobiasahlin/SpinKit)
--- index.js
+++ index.js
@@ -1,38 +1,78 @@
-var program = require('commander');
+var defaultPort = 4000;
+
+
+var fs = require('fs');
+var packageJson = require('./package.json');
+var config = require('./config');
+var pty = require('pty.js');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var CBuffer = require('CBuffer');
+var portfinder = require('portfinder');
 
-var catchupBuffer = new CBuffer(10);
-var updateBuffer = "";
-var currentBuffer = catchupBuffer;
-
-program
- 	.version('0.0.2')
- 	.option('-p, --port <port_number>', 'Port number')
- 	.parse(process.argv);
-
-var port = program.port || 4000;
-
-app.use(express.static(__dirname + '/public'));
-
-io.on('connection', function(socket){
-	io.emit('catchup',currentBuffer.toArray());
+var argv = require('minimist')(process.argv.slice(2),{
+    stopEarly: true,
+    alias: {
+        'p': 'port',
+        'b': 'buffer',
+        'c': 'cols',
+        'r': 'rows',
+        'h': 'help',
+        'v': 'version'
+    },
+    default: {
+        'port': config.defaultPort,
+        'rows': config.defaultRows,
+        'cols': config.defaultCols,
+        'buffer': config.defaultBuffer
+    },
+    boolean: ['help']
 });
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', function(data) {
-    io.emit('consoleUpdate', data);
+if (argv.help) {
+    fs.readFile('./help.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        process.exit(0);
+    });
+}
+else if (argv.version) {
+    console.log(packageJson.version);
+    process.exit(0);
+}
+else {
 
-    currentBuffer.push(data);
+    var catchupBuffer = new CBuffer(argv.buffer);
 
-    console.log(data);
-});
+    console.log(argv.cols);
 
-http.listen(port, function(){
- 	console.log('listening on *:' + port);
-});
+    portfinder.basePort = argv.port;
+    portfinder.getPort(function (err,port) {
+        app.use(express.static(__dirname + '/public'));
+        
+        var term = pty.spawn(argv._[0], argv._.slice(1), {
+            name: 'xterm-color',
+            cols: argv.cols,
+            rows: argv.rows,
+            //cwd: process.env.HOME,
+            //env: process.env
+        });
 
+        term.on('data', function(data) {
+            io.emit('consoleUpdate', data);
+            catchupBuffer.push(data);
+        });
+        
+        io.on('connection', function(socket){
+            io.emit('catchup',catchupBuffer.toArray());
+            term.redraw();
+        });
+
+        http.listen(argv.port, function(){
+            console.log('listening on port ' + argv.port);
+        });
+    });
+
+}
